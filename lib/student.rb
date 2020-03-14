a8f708e99311d16041dca427cb66f47545d05948
replacem3nts <donny.landis@gmail.com>
2020-03-14 17:14:47 -0400

    updates files to pass all tests

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative "../lib/user.rb"
+
+class Student < User
+attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(new_info)
+        @knowledge << new_info
+    end
 
 end
