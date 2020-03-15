fa66daea689be33297e2244a85c9d4a577953fea
Jackmt9 <jackmt9@gmail.com>
2020-03-14 22:21:18 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,5 @@
 class Student < User
 
-    attr_accessor :knowledge
-
     def initialize
         @knowledge = []
     end
@@ -13,5 +11,5 @@ class Student < User
     def knowledge
         @knowledge
     end
-    
+
 end
